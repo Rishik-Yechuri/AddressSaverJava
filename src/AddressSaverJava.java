70cbf7017ef9d2185a313962771f37f8b8b7bb57
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2021-04-30 16:53:33 -0700

    Comments addded

--- src/AddressSaverJava.java
+++ src/AddressSaverJava.java
@@ -19,19 +19,30 @@ import java.nio.file.Paths;
 
 public class AddressSaverJava {
     public static void main(String[] args) throws IOException {
+        //Creates an arraylist of words to replace in the sales document
         ArrayList<String> thingsToReplace = new ArrayList<String>(Arrays.asList("<DATE>","<NAME>","<ADDRESS>","<CITY>","<STATE>","<ZIPCODE>"));
+        //Loops through each person
         for(int y=1;y<=11;y++) {
+            //Gets the data from the Address file
             String line = "";
             try (Stream<String> lines = Files.lines(Paths.get("Address.csv"))) {
                 line = lines.skip(y).findFirst().get();
             } catch (IOException e) {
                 System.out.println(e);
             }
+            //Splits the information into an array
             String[] replacementInfo = line.split(",");
+            //Uses the persons name to create the document name
             String documentName = replacementInfo[0].substring(0,1) + "_" + replacementInfo[1] + "_JAVA";
+            //Copies SalesLetter to TemplateFile
             copyFile(new File("SalesLetter.dat"),new File("TemplateFile.dat"));
+            //Loops through thingsToReplace
             for (int x = 0; x < thingsToReplace.size(); x++) {
+                //Stores what to replace it with
                 String stringReplacement = "";
+                /*Depending on the data field,stringReplacement is set to the correct value
+                For example if the data field is "<NAME>",the replacement will be Billy Bob.
+                But if the data field is "<CITY>,the replacement will be Frisco"*/
                 switch (thingsToReplace.get(x)) {
                     case "<DATE>":
                         Format f = new SimpleDateFormat("MM/dd/yy");
@@ -54,16 +65,20 @@ public class AddressSaverJava {
                         stringReplacement = replacementInfo[5];
                         break;
                 }
+                //Replaces the field with the proper information
                 modifyFile("TemplateFile.dat", thingsToReplace.get(x), stringReplacement);
             }
+            //Changes the "<YOURNAME>" field to "Rishik Yechuri"
             modifyFile("TemplateFile.dat", "<YOURNAME>", "Rishik Yechuri");
+            //Converts TemplateFile to a byte array
             byte[] bytes = Files.readAllBytes(Paths.get("TemplateFile.dat"));
-           // String nameOfPDF = "Output" + y;
+            //Creates a new file using the modified TemplateFile
             OutputStream out = new FileOutputStream(documentName);
             out.write(bytes);
             out.close();
         }
     }
+    //A function to copy a file to another file
     public static void copyFile(File sourceFile, File destFile) throws IOException {
         if(!destFile.exists()) {
             destFile.createNewFile();
@@ -88,6 +103,7 @@ public class AddressSaverJava {
             }
         }
     }
+    //A function to replace a string in a file with another string
     public static void modifyFile(String filePath, String oldString, String newString)
     {
         File fileToBeModified = new File(filePath);
